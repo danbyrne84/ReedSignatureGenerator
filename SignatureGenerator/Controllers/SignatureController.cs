9aac5257f105b2dc63b044219d9e62aeb80c1fa1
Dan Byrne <dan@danielbyrne.net>
2021-01-05 22:05:20 +0000

    Add project files.

---
+++ SignatureGenerator/Controllers/SignatureController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SignatureGenerator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SignatureController : ControllerBase
+    {
+        public SignatureController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get([FromQuery]string token, [FromQuery]string stringToSign)
+        {
+            var hmacsha1 = new HMACSHA1(new Guid(token).ToByteArray());  // instantiate HMAC SHA1 with byte[]
+            byte[] byteArray = System.Text.Encoding.ASCII.GetBytes(stringToSign);
+            var computedHash = hmacsha1.ComputeHash(byteArray);
+
+            var calculatedSignature = Convert.ToBase64String(computedHash);
+
+            return calculatedSignature;
+        }
+    }
+}

67bf26470ad81c666ff3881be1bc415a575332c9
Dan Byrne <dan@danielbyrne.net>
2021-01-05 22:35:50 +0000

    Replaced with 2.2

--- Controllers/SignatureController.cs
+++ Controllers/SignatureController.cs
@@ -4,21 +4,15 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
-namespace SignatureGenerator.Controllers
+namespace ReedSignatureGenerator.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class SignatureController : ControllerBase
     {
-        public SignatureController()
-        {
-            
-        }
-
         [HttpGet]
-        public string Get([FromQuery]string token, [FromQuery]string stringToSign)
+        public string Get([FromQuery] string token, [FromQuery] string stringToSign)
         {
             var hmacsha1 = new HMACSHA1(new Guid(token).ToByteArray());  // instantiate HMAC SHA1 with byte[]
             byte[] byteArray = System.Text.Encoding.ASCII.GetBytes(stringToSign);
